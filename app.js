8c7c4c66f94d926fc14e7983a332e4f48749071b
Nghi Tran <nghi.t.tran8@gmail.com>
2020-01-22 12:00:43 -0800

    added authorization

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
 // require local depencies
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const helmet = require('helmet');
+// load data
 const movies = require('./movie.js');
 
+// invoke express to create object with express methods
 const app = express();
-app.use(morgan('dev'));
-app.use(cors());
 
-app.get('/movie', (req, res) => {
+// configure middleware
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors());
+app.use(handleAuth);
+
+// middleware
+function handleAuth(req, res, next) {
+  // if authorization not provided, insert empty string
+  const bearToken = req.get('Authorization') || '';
+  // get stored token
+  const apiToken = process.env.API_TOKEN;
+  // if token does not match, return error code
+  if(apiToken !== bearToken.split(' ')[1]) {
+    return res.status(401).json({ error: 'Unauthorized request'});
+  }
+  // else move on to callback
+  next();
+}
+
+// callback
+function handleMovieFilter(req, res) {
   const {genre, country, avg_vote} = req.query;
   let filteredMovies = [ ...movies ];
   // genre
@@ -22,16 +45,15 @@ app.get('/movie', (req, res) => {
   }
 
   // country
-
-if(country){
-  if(!filteredMovies.find(each=>each.country.includes(country))){
-    
-     return res.status(400).json({error:"Country not found"})
+  if(country){
+    // if country doesn't exist, return error code
+    if(!filteredMovies.find(each=>each['country'].toLowerCase().includes(country.toLowerCase()))){
+      return res.status(400).json({error:'Country not found'});
+    }
+    // filter for country
+    filteredMovies = filteredMovies.filter(movie => movie['country'].toLowerCase().includes(country.toLowerCase()));
   }
-return res.status(200).send(`Found ${country}`)
-}
-
-  }
+  
 
   // avg_vote
   if(avg_vote){
@@ -45,10 +67,10 @@ return res.status(200).send(`Found ${country}`)
     // filter movie for avg vote >= the specified vote
     filteredMovies = filteredMovies.filter(movie => movie['avg_vote'] >= Number(avg_vote));
   }
-
-
   res.json(filteredMovies);
-});
+}
+
+app.get('/movie', handleMovieFilter);
 
 app.listen(8001, () => {
   console.log('listening on port 8001');
